1a862ba5271ae79cd1ec3d5da10f92201d000fb3
Tom van Dijk <18gatenmaker6@gmail.com>
2025-05-19 10:25:41 +0200

    feather: disable vscode

--- home/feather.nix
+++ home/feather.nix
@@ -2,11 +2,11 @@
 {
   imports = [
     # modules/hyprland.nix
-    modules/vscode.nix
+    # modules/vscode.nix
   ];
 
-  modules.vscode = {
-    enable = true;
-    gimmeGimmeGimme = true;
-  };
+  # modules.vscode = {
+  #   enable = true;
+  #   gimmeGimmeGimme = true;
+  # };
 }
