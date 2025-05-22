784d02c3a94f59f8e4cb2d9a8e9c376dfce07565
Tom van Dijk <18gatenmaker6@gmail.com>
2025-05-22 13:50:34 +0200

    localsend-rs: add sopsBootstrap option

--- os/modules/localsend-rs.nix
+++ os/modules/localsend-rs.nix
@@ -1,31 +1,38 @@
 {
-  lib,
-  inputs,
   config,
+  inputs,
+  lib,
   ...
 }:
+let
+  cfg = config.services.localsend-rs;
+in
 {
   imports = [
     inputs.localsend-rs.nixosModules.localsend-rs
   ];
 
-  services.localsend-rs = {
-    enable = lib.mkDefault false;
-    user = "tomvd";
-  };
+  options.services.localsend-rs.sopsBootstrap = lib.mkEnableOption "only setup nix and sops, don't try to fetch localsend-rs yet";
 
-  # to access the repo
-  nix = {
-    extraOptions = ''
-      !include ${config.sops.secrets.localsend-rs.path}
-    '';
-  };
+  config = {
+    services.localsend-rs = lib.mkIf (!cfg.sopsBootstrap) {
+      enable = lib.mkDefault false;
+      user = "tomvd";
+    };
 
-  sops.secrets.localsend-rs = {
-    mode = "0440";
-    sopsFile = ../../secrets/localsend-rs.secret;
-    format = "binary";
-    owner = "tomvd";
-    group = "users";
+    # to access the repo
+    nix = {
+      extraOptions = ''
+        !include ${config.sops.secrets.localsend-rs.path}
+      '';
+    };
+
+    sops.secrets.localsend-rs = {
+      mode = "0440";
+      sopsFile = ../../secrets/localsend-rs.secret;
+      format = "binary";
+      owner = "tomvd";
+      group = "users";
+    };
   };
 }
