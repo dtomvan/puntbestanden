c774506876fc63cffa59d73bc88bc865de15f158
Tom van Dijk <18gatenmaker6@gmail.com>
2025-05-25 21:05:43 +0200

    firefox: remove examenblad bookmarks

--- home/modules/firefox.nix
+++ home/modules/firefox.nix
@@ -148,71 +148,46 @@ in
       bookmarks.settings = [
         {
           toolbar = true;
-          bookmarks =
-            [
-              {
-                name = "about:config";
-                url = "about:config";
-              }
-              {
-                name = "nixpkgs";
-                url = "https://github.com/NixOS/nixpkgs/";
-              }
-              # {
-              #   name = "NGI@NIX";
-              #   toolbar = true;
-              #   bookmarks = [
-              # {
-              #   name = "SoN 2025";
-              #   url = "https://github.com/ngi-nix/summer-of-nix/";
-              # }
-              # {
-              #   name = "ngipkgs";
-              #   url = "https://github.com/ngi-nix/ngipkgs/";
-              # }
-              #   ];
-              # }
-              {
-                name = "Revsets";
-                url = "https://jj-vcs.github.io/jj/latest/revsets/";
-              }
-              {
-                name = "unreviewed PRs to nixpkgs";
-                url = "https://github.com/NixOS/nixpkgs/pulls?utf8=%E2%9C%93&q=is%3Aopen+is%3Apr+-is%3Adraft+review%3Anone+sort%3Acreated-asc+-label%3A%222.status%3A+work-in-progress%22+-label%3A%222.status%3A+merge+conflict%22";
-              }
-              {
-                name = "nixpkgs low-hanging fruit";
-                url = "https://github.com/NixOS/nixpkgs/pulls?q=is%3Aopen+is%3Apr+-is%3Adraft+review%3Anone+sort%3Acreated-asc+-label%3A%222.status%3A+work-in-progress%22+-label%3A%222.status%3A+merge+conflict%22+-label%3A%222.status%3A+stale%22+";
-              }
-              {
-                name = "Nix package versions";
-                url = "https://lazamar.co.uk/nix-versions/";
-              }
-            ]
-            ++ (builtins.map
-              (v: {
-                name = v;
-                url = "https://www.examenblad.nl/2024/vwo/vakken/exacte-vakken/${v}";
-              })
-              [
-                "biologie-vwo"
-                "natuurkunde-vwo"
-                "scheikunde-vwo"
-                "wiskunde-b-vwo"
-              ]
-            )
-            ++ (builtins.map
-              (v: {
-                name = v;
-                url = "https://www.examenblad.nl/2024/vwo/vakken/talen/${v}";
-              })
-              [
-                "engels-vwo"
-                "frans-vwo"
-                "griekse-taal-cultuur-vwo"
-                "nederlands-vwo"
-              ]
-            );
+          bookmarks = [
+            {
+              name = "about:config";
+              url = "about:config";
+            }
+            {
+              name = "nixpkgs";
+              url = "https://github.com/NixOS/nixpkgs/";
+            }
+            # {
+            #   name = "NGI@NIX";
+            #   toolbar = true;
+            #   bookmarks = [
+            # {
+            #   name = "SoN 2025";
+            #   url = "https://github.com/ngi-nix/summer-of-nix/";
+            # }
+            # {
+            #   name = "ngipkgs";
+            #   url = "https://github.com/ngi-nix/ngipkgs/";
+            # }
+            #   ];
+            # }
+            {
+              name = "Revsets";
+              url = "https://jj-vcs.github.io/jj/latest/revsets/";
+            }
+            {
+              name = "unreviewed PRs to nixpkgs";
+              url = "https://github.com/NixOS/nixpkgs/pulls?utf8=%E2%9C%93&q=is%3Aopen+is%3Apr+-is%3Adraft+review%3Anone+sort%3Acreated-asc+-label%3A%222.status%3A+work-in-progress%22+-label%3A%222.status%3A+merge+conflict%22";
+            }
+            {
+              name = "nixpkgs low-hanging fruit";
+              url = "https://github.com/NixOS/nixpkgs/pulls?q=is%3Aopen+is%3Apr+-is%3Adraft+review%3Anone+sort%3Acreated-asc+-label%3A%222.status%3A+work-in-progress%22+-label%3A%222.status%3A+merge+conflict%22+-label%3A%222.status%3A+stale%22+";
+            }
+            {
+              name = "Nix package versions";
+              url = "https://lazamar.co.uk/nix-versions/";
+            }
+          ];
         }
       ];
 
