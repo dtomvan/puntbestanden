560cc8df2ea466313b1f53b8d18226be386126fb
Tom van Dijk <18gatenmaker6@gmail.com>
2025-06-03 19:43:37 +0200

    flake.nixConfig: factor out

--- flake.nix
+++ flake.nix
@@ -1,17 +1,8 @@
 rec {
   description = "Home Manager configuration of tomvd";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://cache.garnix.io"
-    ];
-
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-    ];
-  };
+  # HACK: I would do `nixConfig = import ./nix-config.nix` here, but that IS
+  # NOT POSSIBLE >:'(
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
