477d6a9f4b205324776423704720d1858355359b
Tom van Dijk <18gatenmaker6@gmail.com>
2025-06-03 19:49:11 +0200

    fix: remove old default.nix

--- default.nix
+++
@@ -1,9 +0,0 @@
-{
-  system ? builtins.currentSystem,
-  pkgs ? import <nixpkgs> { inherit system; }, # on one of my nixos configs, this is just always flake nixpkgs, otherwise good luck
-  ...
-}:
-(pkgs.lib.filesystem.packagesFromDirectoryRecursive {
-  inherit (pkgs) callPackage newScope;
-  directory = ./packages/by-name;
-})
