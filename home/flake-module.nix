2f38fad2265482c3c7d48daa6e6e8f6577a5a696
Tom van Dijk <18gatenmaker6@gmail.com>
2025-06-03 19:51:41 +0200

    flake.home: factor out

---
+++ home/flake-module.nix
@@ -0,0 +1,22 @@
+{ self, inputs, ... }:
+with inputs.nixpkgs.lib;
+let
+  mkPkgs = system: import ../lib/make-packages.nix { inherit self system inputs; };
+
+  makeHome =
+    _key: host:
+    nameValuePair "tomvd@${host.hostName}" (
+      inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = mkPkgs host.system;
+
+        modules = import ./modules.nix { inherit host inputs; };
+
+        extraSpecialArgs = {
+          inherit host;
+        };
+      }
+    );
+in
+{
+  flake.homeConfigurations = mapAttrs' makeHome (import ../hosts.nix);
+}
