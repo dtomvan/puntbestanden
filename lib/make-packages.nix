f0ace803fabe131a315b264cf137f9a95958b2ec
Tom van Dijk <18gatenmaker6@gmail.com>
2025-06-03 21:58:54 +0200

    pkgs: remove firefox-deveditin-bin overlay
    Not needed anymore, nixpkgs is newer

--- lib/make-packages.nix
+++ lib/make-packages.nix
@@ -14,17 +14,5 @@ import nixpkgs {
     # broken
     # (_final: _prev: { nix4vscode.forOpenVsx = inputs.nix4vscode.lib.${system}.forOpenVsx; })
     (_final: _prev: self.packages.${system})
-    (_final: prev: {
-      # https://github.com/dtomvan/puntbestanden/issues/1
-      # https://github.com/NixOS/nixpkgs/pull/409162/files
-      firefox-devedition-bin-unwrapped = (
-        prev.firefox-devedition-bin-unwrapped.overrideAttrs {
-          src = prev.fetchurl {
-            url = "https://archive.mozilla.org/pub/devedition/releases/139.0b10/linux-x86_64/en-US/firefox-139.0b10.tar.xz";
-            sha256 = "45c364b418befa59cbd1273a4152a31b1bca8873d8952ca1c0870c6f38a924f5";
-          };
-        }
-      );
-    })
   ];
 }
