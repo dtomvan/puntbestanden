caf6ebf4432f3ab41aeaab2376f88eebf7fb7f0f
Tom van Dijk <18gatenmaker6@gmail.com>
2025-06-03 18:19:59 +0200

    flake: factor out mkpkgs

--- flake.nix
+++ flake.nix
@@ -99,32 +99,7 @@ rec {
         flake =
           with nixpkgs.lib;
           let
-            mkPkgs =
-              system:
-              import nixpkgs {
-                inherit system;
-                config.allowUnfree = true;
-                overlays = [
-                  inputs.nur.overlays.default
-                  inputs.nix4vscode.overlays.forVscode
-                  inputs.lazy-apps.overlays.default
-                  # broken
-                  # (_final: _prev: { nix4vscode.forOpenVsx = inputs.nix4vscode.lib.${system}.forOpenVsx; })
-                  (_final: _prev: self.packages.${system})
-                  (_final: prev: {
-                    # https://github.com/dtomvan/puntbestanden/issues/1
-                    # https://github.com/NixOS/nixpkgs/pull/409162/files
-                    firefox-devedition-bin-unwrapped = (
-                      prev.firefox-devedition-bin-unwrapped.overrideAttrs {
-                        src = prev.fetchurl {
-                          url = "https://archive.mozilla.org/pub/devedition/releases/139.0b10/linux-x86_64/en-US/firefox-139.0b10.tar.xz";
-                          sha256 = "45c364b418befa59cbd1273a4152a31b1bca8873d8952ca1c0870c6f38a924f5";
-                        };
-                      }
-                    );
-                  })
-                ];
-              };
+            mkPkgs = system: import ./lib/make-packages.nix { inherit system nixpkgs inputs; };
 
             getHosts = type: filterAttrs (_k: v: hasInfix type v.system) (import ./hosts.nix);
 
