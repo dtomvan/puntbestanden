e59c236233260fb3a3fe5df17bae7668c74e76c6
Tom van Dijk <18gatenmaker6@gmail.com>
2025-07-07 00:47:07 +0200

    home(neovim): get rid of neovim-qt

--- home/modules/neovim/default.nix
+++ home/modules/neovim/default.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   lib,
   config,
   ...
@@ -15,8 +14,6 @@ in
       nixd.enable = lib.mkEnableOption "use nixd with this flake";
       lazyMoar = lib.mkEnableOption "install a lot of LSP servers, but lazily with lazy-apps";
     };
-
-    enableQt = lib.mkEnableOption "QT GUI";
   };
 
   imports = [
@@ -26,8 +23,6 @@ in
   ];
 
   config = lib.mkIf cfg.enable {
-    home.packages = lib.optional cfg.enableQt pkgs.neovim-qt;
-
     programs.nixvim = {
       enable = true;
       defaultEditor = true;
