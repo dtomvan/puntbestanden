45868bd6de4f5146d12cc591bb5e62303f617dd1
Tom van Dijk <18gatenmaker6@gmail.com>
2025-07-07 02:57:43 +0200

    chore: move vintagestory 1.20 to .net 8
    I've patched vintagestory 1.20 to be forced to use .net 8 in vs2nix,
    hence the update causes this. Hence I removed the
    `permittedInsecurePackages` because it's not required to run
    vintagestory anymore.

--- lib/make-packages.nix
+++ lib/make-packages.nix
@@ -4,16 +4,10 @@
   system,
   nixpkgs ? inputs.nixpkgs,
 }:
-let
-  inherit (nixpkgs) lib;
-  inherit (nixpkgs.legacyPackages.${system}) vintagestory;
-in
 import nixpkgs {
   inherit system;
   config = {
     allowUnfree = true;
-    # for vintagestory
-    permittedInsecurePackages = lib.optional (lib.versionOlder vintagestory.version "1.21") "dotnet-runtime-7.0.20";
   };
   overlays = [
     inputs.nur.overlays.default
