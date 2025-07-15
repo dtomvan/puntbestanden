fc8e16e44b7d20100513af2915c29ad170bd3175
Tom van Dijk <18gatenmaker6@gmail.com>
2025-07-15 22:09:13 +0200

    feat: write in-tree editorconfig as in home-manager

--- modules/dev/editorconfig.nix
+++ modules/dev/editorconfig.nix
@@ -1,13 +1,28 @@
+let
+  settings = {
+    "*.nix" = {
+      indent_style = "space";
+      indent_size = 2;
+    };
+  };
+in
 {
   flake.modules.homeManager.profiles-base = {
     editorconfig = {
       enable = true;
-      settings = {
-        "*.nix" = {
-          indent_style = "space";
-          indent_size = 2;
-        };
-      };
+      inherit settings;
     };
   };
+
+  # sync the editorconfig to this tree.
+  perSystem =
+    { pkgs, ... }:
+    {
+      files.files = [
+        {
+          path_ = ".editorconfig";
+          drv = (pkgs.formats.ini { }).generate ".editorconfig" settings;
+        }
+      ];
+    };
 }
