aa155ebc41f8434c5b95086f5162e03081af4e5a
Tom van Dijk <18gatenmaker6@gmail.com>
2025-07-29 00:21:34 +0200

    home: add script to upload screenshot from clipboard to bart

--- modules/webapps/copyparty/bart.nix
+++ modules/webapps/copyparty/bart.nix
@@ -1,8 +1,41 @@
+let
+  url = "https://fs.omeduostuurcentenneef.nl/drop/";
+in
 {
   flake.webApps.copyparty_omeduo = {
     name = "Omeduo drop";
-    url = "https://fs.omeduostuurcentenneef.nl/drop/";
+    inherit url;
     icon = ./favicon.png;
     genericName = "copyparty Instance";
   };
+
+  # yeahhh I guess this is the best place for this
+  flake.modules.homeManager.profiles-base =
+    { pkgs, ... }:
+    {
+      home.packages = [
+        (pkgs.writeShellApplication {
+          name = "duo";
+
+          runtimeInputs = with pkgs; [
+            coreutils
+            curl
+            gnugrep
+            wl-clipboard
+          ];
+
+          text = ''
+            if ! wl-paste -l | grep '^image/'; then
+              echo not an image
+              exit 1
+            fi
+
+            extension="$(wl-paste | file -b --extension -)"
+            wl-paste \
+              | curl -sT- ${url}."$extension"?want=url \
+              | tee >(wl-copy)
+          '';
+        })
+      ];
+    };
 }
