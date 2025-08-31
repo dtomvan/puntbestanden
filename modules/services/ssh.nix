a3908c88397fbcdd086c0da73854c2a2512f5f4d
Tom van Dijk <18gatenmaker6@gmail.com>
2025-08-31 14:48:53 +0200

    fix: correct SSH keys

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -4,8 +4,8 @@
     with lib;
     let
       keys = {
-        boomer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFkdbc1mWvIUciwrrIZVRYrZwTBmQ7Cehd/laxzzdlyL tomvd@boomer";
-        feather = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICYRKHdw4dNFdV36OmEQvxEvko5fpkfV/Ch6pQypmOo8 tomvd@feather";
+        boomer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8M2MZ3h6AGyUGmzIY5AG0nRYvh6DOAE4TbEmfSefdt tomvd@boomer";
+        feather = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILZCTsfqR/RkuQxq4Z2Pq6PeLW/QqRQwM6JTluy34XyV tomvd@feather";
         kaput = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+tAFrOJ+6ksJJ2WcQec0xbyCEb+zYfDtLdyVTOW8Vg tomvd@kaput";
       };
       knownHosts = mapAttrs' (n: v: nameValuePair n { publicKey = v; }) keys;
