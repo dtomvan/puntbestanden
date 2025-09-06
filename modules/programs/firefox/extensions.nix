d0e9e40d09b81188916e55e18f6df69c5bf33a4a
Tom van Dijk <18gatenmaker6@gmail.com>
2025-09-06 15:09:28 +0200

    feat(firefox): v139 native vertical tabs and privacy settings

--- modules/programs/firefox/extensions.nix
+++ modules/programs/firefox/extensions.nix
@@ -1,6 +1,9 @@
 {
   flake.modules.homeManager.firefox =
     { pkgs, ... }:
+    let
+      ubo = pkgs.nur.repos.dtomvan.ublock-origin;
+    in
     {
       programs.firefox.profiles.default = {
         extensions = with pkgs.nur.repos.rycee.firefox-addons; {
@@ -9,17 +12,22 @@
             sponsorblock
             dearrow
             plasma-integration
+
+            ubo
           ]
           ++ (with pkgs.nur.repos.dtomvan; [
             darkreader
             obsidian-web-clipper
-            sidebery
             steam-database
-            ublock-origin
             enhancer-for-youtube-bin
           ]);
 
           force = true;
+        };
+
+        settings = {
+          "sidebar.main.tools" = "${ubo.addonId},history,bookmarks";
+          "sidebar.installed.extensions" = ubo.addonId;
         };
       };
     };
--- modules/programs/firefox/prefs.nix
+++ modules/programs/firefox/prefs.nix
@@ -7,6 +7,10 @@
         "extensions.activeThemeID" = "default-theme@mozilla.org";
         "general.autoScroll" = true;
 
+        # native vertical tabs!
+        "sidebar.verticalTabs" = true;
+        "sidebar.revamp" = true;
+
         "browser.search.region" = "NL";
         "browser.search.isUS" = false;
         "distribution.searchplugins.defaultLocale" = "nl-NL";
@@ -25,13 +29,27 @@
         "browser.engagement.ctrlTab.has-used" = true;
         "browser.engagement.downloads-button.has-used" = true;
         "browser.engagement.fxa-toolbar-menu-button.has-used" = true;
+        "browser.engagement.sidebar-button.has-used" = true;
+        "browser.engagement.home-button.has-used" = true;
+        "browser.engagement.library-button.has-used" = true;
+        "sidebar.notification.badge.aichat" = false;
+        "browser.ml.chat.sidebar" = false;
+        "sidebar.verticalTabs.dragToPinPromo.dismissed" = true;
+        "sidebar.new-sidebar.has-used" = true;
         "browser.newtabpage.enabled" = false;
         "browser.discovery.enabled" = false;
+        "browser.tabs.groups.smart.userEnabled" = false; # don't use AI for tab group names bruh...
+        "browser.newtabpage.activity-stream.showSponsoredCheckboxes" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
         "trailhead.firstrun.didSeeAboutWelcome" = true;
         "browser.translations.automaticallyPopup" = false;
+        "browser.urlbar.suggest.trending" = false; # "trending search suggestions" fuck off
 
         # don't track me please
         "privacy.globalprivacycontrol.enabled" = true;
+
+        # disable "quick actions" during search
+        "browser.urlbar.suggest.quickactions" = false;
       };
 
       extraConfig = ''
