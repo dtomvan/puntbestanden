123b334059d4960d000c923c51c1dbf197e828d1
Tom van Dijk <18gatenmaker6@gmail.com>
2025-11-16 14:42:32 +0100

    fix(minecraft): eval error in upstream minecraft-plymouth-theme

--- flake.nix
+++ flake.nix
@@ -128,7 +128,7 @@
           follows = "nixpkgs";
         };
       };
-      url = "github:dtomvan/minecraft-plymouth-theme";
+      url = "github:dtomvan/minecraft-plymouth-theme/bb7f46cf3ea83c80604fb1003abbfd4c2cd4312e";
     };
     minegrub-theme = {
       inputs = {
