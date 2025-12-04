3b4386a94b54869b381694875467421bf23dbec1
Tom van Dijk <18gatenmaker6@gmail.com>
2025-12-04 19:58:59 +0100

    fix(dev): migrate lisp module to nixvim class

--- modules/dev/lisp.nix
+++ modules/dev/lisp.nix
@@ -1,32 +1,31 @@
 {
-  flake.modules.homeManager.lisp =
+  flake.modules.nixvim.lisp =
+    { pkgs, ... }:
     {
-      pkgs,
-      ...
-    }:
-    {
-      programs.nixvim = {
-        plugins = {
-          conjure.enable = true;
-          lsp.servers = {
-            clojure_lsp.enable = true;
-            fennel_ls.enable = true;
-          };
+      plugins = {
+        conjure.enable = true;
+        lsp.servers = {
+          clojure_lsp.enable = true;
+          fennel_ls.enable = true;
         };
-
-        extraPlugins = [
-          (pkgs.vimUtils.buildVimPlugin {
-            name = "vim-jack-in";
-            src = pkgs.fetchFromGitHub {
-              owner = "clojure-vim";
-              repo = "vim-jack-in";
-              rev = "45e5293cff0802a51dbed31a9b0141b0f80e2952";
-              hash = "sha256-4kYY0jUv5U2i+G/29vPOZHqUX/cEQTWBo9pghRg9gIw=";
-            };
-          })
-        ];
       };
 
+      extraPlugins = [
+        (pkgs.vimUtils.buildVimPlugin {
+          name = "vim-jack-in";
+          src = pkgs.fetchFromGitHub {
+            owner = "clojure-vim";
+            repo = "vim-jack-in";
+            rev = "45e5293cff0802a51dbed31a9b0141b0f80e2952";
+            hash = "sha256-4kYY0jUv5U2i+G/29vPOZHqUX/cEQTWBo9pghRg9gIw=";
+          };
+        })
+      ];
+    };
+
+  flake.modules.homeManager.lisp =
+    { pkgs, ... }:
+    {
       home.packages = with pkgs; [
         clojure
         clojure-lsp
