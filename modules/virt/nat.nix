31ba03e43148be7c01a0d98f53f450d2a9f2a3e8
Tom van Dijk <18gatenmaker6@gmail.com>
2026-01-24 12:30:30 +0100

    feat(virt-kvm): enable nat for other hosts too

--- modules/virt/nat.nix
+++ modules/virt/nat.nix
@@ -1,13 +1,23 @@
-{ lib, ... }:
+{ self, lib, ... }:
 {
   # TODO: not put this under kvm
-  flake.modules.nixos.virt-kvm.networking.nat = {
-    enable = true;
-    # Use "ve-*" when using nftables instead of iptables
-    internalInterfaces = [ "ve-+" ];
-    # TODO: update for other machines if needed
-    externalInterface = lib.mkDefault "wlp7s0";
-    # Lazy IPv6 connectivity for the container
-    enableIPv6 = true;
-  };
+  flake.modules.nixos.virt-kvm =
+    { config, ... }:
+    let
+      host = self.lib.getHost {
+        inherit config;
+        module = "virt-kvm/nat";
+      };
+    in
+    {
+      networking.nat = {
+        enable = true;
+        # Use "ve-*" when using nftables instead of iptables
+        internalInterfaces = [ "ve-+" ];
+        # TODO: update for other machines if needed
+        externalInterface = lib.mkIf (host ? wirelessInterface) host.wirelessInterface;
+        # Lazy IPv6 connectivity for the container
+        enableIPv6 = true;
+      };
+    };
 }
