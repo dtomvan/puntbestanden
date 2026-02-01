f4cf9fc203765be1437b40f32eb7e8cbb7e7b24a
Tom van Dijk <18gatenmaker6@gmail.com>
2026-02-01 03:31:59 +0100

    feat(mangowc): port my old i3lock config to swaylock-effects

--- modules/profiles/mangowc.nix
+++ modules/profiles/mangowc.nix
@@ -7,16 +7,56 @@
 
   # use mangowc from nixpkgs
   perSystem =
-    { pkgs, ... }:
+    { pkgs, self', ... }:
     {
       packages = {
+        swaylock = pkgs.writeShellScriptBin "swaylock" ''
+          B='#00000000' # blank
+          C='#ffffff22' # clear ish
+          D='#81a1c1cc' # default
+          T='#88c0d0ee' # text
+          W='#bf616aee' # wrong
+          V='#5e81acbb' # verifying
+          L='#4c566aaa' # inside
+
+          ${pkgs.swaylock-effects}/bin/swaylock \
+              -n \
+              --color="#00000000" \
+              --inside-ver-color=$C \
+              --ring-ver-color=$V \
+              \
+              --inside-wrong-color=$C \
+              --ring-wrong-color=$W \
+              \
+              --inside-color=$L \
+              --ring-color=$D \
+              --line-color=$B \
+              --separator-color=$D \
+              \
+              --text-ver-color=$T \
+              --text-wrong-color=$T \
+              --text-color=$T \
+              \
+              --key-hl-color=$L \
+              --bs-hl-color=$W \
+              \
+              --clock \
+              --indicator \
+              --screenshots \
+              --timestr="%I:%M:%S %p" \
+              --datestr="%A, %d-%m-%Y" \
+              --font='JetBrainsMono Nerd Font' \
+              --font-size=20 \
+              --effect-blur=10x3 \
+              --fade-in=1
+        '';
         mango = pkgs.mangowc;
         hypridle = pkgs.writeShellApplication {
           name = "hypridle";
           runtimeInputs = with pkgs; [
             nur.repos.dtomvan.wlr-dpms
             hypridle
-            swaylock
+            self'.packages.swaylock
           ];
           # HACK: the promised `-c` flag for hypridle doesn't actually work.
           # Making a fake XDG_CONFIG_HOME.
