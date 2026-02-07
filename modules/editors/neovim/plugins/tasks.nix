876d09e261ffcd27e6c33511598fd630df03de80
Tom van Dijk <18gatenmaker6@gmail.com>
2026-02-07 17:47:50 +0100

    feat(neovim): update tasks
    backlinks support!

--- modules/editors/neovim/plugins/tasks.nix
+++ modules/editors/neovim/plugins/tasks.nix
@@ -6,6 +6,16 @@
   };
   flake.modules.nixvim.default = {
     imports = [ inputs.tasks.modules.nixvim.default ];
+    # TODO: write as nixvim-native keys
+    extraConfigLuaPost = ''
+      vim.keymap.set("n", "<leader>tg", require'tasks'.go_to)
+      vim.keymap.set("n", "<leader>tn", require'tasks'.create_from_todo)
+      vim.keymap.set("n", "<leader>tc", require'tasks'.new)
+      vim.keymap.set("n", "<leader>tl", require'tasks'.list)
+      vim.keymap.set("n", "<leader>tq", require'tasks'.qf_list)
+      vim.keymap.set("n", "<leader>to", "<cmd>Telescope tasks<cr>")
+      vim.keymap.set("n", "<leader>tb", "<cmd>Telescope tasks backlinks<cr>")
+    '';
     plugins.tasks = {
       enable = true;
       settings.add_commands = true;
