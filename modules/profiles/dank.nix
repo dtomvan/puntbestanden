5f6b0b302ee00234ebf156ff17c0758e6d7388ce
Tom van Dijk <18gatenmaker6@gmail.com>
2026-02-15 14:44:07 +0100

    chore: nix fmt

--- modules/profiles/dank.nix
+++ modules/profiles/dank.nix
@@ -1,32 +1,31 @@
 # WARNING: plasma theming might break, so don't use with profiles-plasma, I guess
 { lib, ... }:
 {
-  flake.modules.nixos.profiles-dank =
-    {
-      programs.niri.enable = true;
+  flake.modules.nixos.profiles-dank = {
+    programs.niri.enable = true;
 
-      services.displayManager = {
-        dms-greeter = {
-          enable = true;
-          compositor.name = "niri";
-        };
-        sddm.enable = lib.mkForce false;
-        sddm.wayland.enable = lib.mkForce false;
-        gdm.enable = lib.mkForce false;
-      };
-
-      programs.dms-shell = {
+    services.displayManager = {
+      dms-greeter = {
         enable = true;
-        systemd.enable = false;
+        compositor.name = "niri";
       };
-
-      # so dms can write its configs for qt apps
-      # qt = {
-      #   enable = true;
-      #   platformTheme = "qt5ct"; # also does qt6ct
-      #   style = lib.mkForce null; # so qt5ct can be overridden by dms
-      # };
+      sddm.enable = lib.mkForce false;
+      sddm.wayland.enable = lib.mkForce false;
+      gdm.enable = lib.mkForce false;
     };
+
+    programs.dms-shell = {
+      enable = true;
+      systemd.enable = false;
+    };
+
+    # so dms can write its configs for qt apps
+    # qt = {
+    #   enable = true;
+    #   platformTheme = "qt5ct"; # also does qt6ct
+    #   style = lib.mkForce null; # so qt5ct can be overridden by dms
+    # };
+  };
 
   flake.modules.homeManager.profiles-dank =
     { lib, ... }:
