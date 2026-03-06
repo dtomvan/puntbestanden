0a288949e4244260f7c40924ccf47a8dab4904b1
Tom van Dijk <18gatenmaker6@gmail.com>
2026-03-06 22:33:43 +0100

    chore(base): remove redundant TASK(20260204-235727) comment

--- modules/profiles/base.nix
+++ modules/profiles/base.nix
@@ -23,7 +23,7 @@
 
           boot-systemd-boot
           users-root
-          users-tomvd # TASK(20260204-235727): this is required by services-ssh, remove explicit tomvd keys in there
+          users-tomvd
 
           services-ssh
 
