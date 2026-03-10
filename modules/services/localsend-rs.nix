d0cd6cdcd5d07fc3f48616494da2903645de96ac
Tom van Dijk <18gatenmaker6@gmail.com>
2026-03-10 18:48:38 +0100

    refactor(localsend-rs): move sops secret to modules.nixos.sops

--- modules/services/localsend-rs.nix
+++ modules/services/localsend-rs.nix
@@ -23,42 +23,33 @@
       `nix.conf` and you can `nix flake update` for example without any manual setup
     '';
 
-  flake.modules.nixos.services-localsend-rs =
+  flake.modules.nixos.services-localsend-rs = {
+    imports = [ inputs.localsend-rs.nixosModules.localsend-rs ];
+
+    config = {
+      services.localsend-rs = {
+        enable = true;
+        user = "tomvd";
+      };
+    };
+  };
+
+  flake.modules.nixos.sops =
+    { config, ... }:
     {
-      config,
-      lib,
-      ...
-    }:
-    let
-      cfg = config.services.localsend-rs;
-    in
-    {
-      imports = [
-        inputs.localsend-rs.nixosModules.localsend-rs
-      ];
+      # to access the repo
+      nix = {
+        extraOptions = ''
+          !include ${config.sops.secrets.localsend-rs.path}
+        '';
+      };
 
-      options.services.localsend-rs.sopsBootstrap = lib.mkEnableOption "only setup nix and sops, don't try to fetch localsend-rs yet";
-
-      config = {
-        services.localsend-rs = lib.mkIf (!cfg.sopsBootstrap) {
-          enable = lib.mkDefault true;
-          user = "tomvd";
-        };
-
-        # to access the repo
-        nix = {
-          extraOptions = ''
-            !include ${config.sops.secrets.localsend-rs.path}
-          '';
-        };
-
-        sops.secrets.localsend-rs = {
-          mode = "0440";
-          sopsFile = ../../secrets/localsend-rs.secret;
-          format = "binary";
-          owner = "tomvd";
-          group = "users";
-        };
+      sops.secrets.localsend-rs = {
+        mode = "0440";
+        sopsFile = ../../secrets/localsend-rs.secret;
+        format = "binary";
+        owner = "tomvd";
+        group = "users";
       };
     };
 }
