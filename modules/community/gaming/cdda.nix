7ff75da07eeb877ba77687112d4f684d23b8826b
Tom van Dijk <18gatenmaker6@gmail.com>
2026-03-26 13:51:42 +0100

    feat(cdda): add damalskSoundpack

--- modules/community/gaming/cdda.nix
+++ modules/community/gaming/cdda.nix
@@ -8,9 +8,19 @@
       myCddaPackages = lib.recurseIntoAttrs (
         pkgs.callPackage "${inputs.nixpkgs-cdda.outPath}/pkgs/games/cataclysm-dda" { }
       );
+      damalskSoundpack = myCddaPackages.pkgs.buildSoundPack {
+        modName = "@'s Soundpack";
+        version = "0-unstable-2019-06-07";
+        src = pkgs.fetchFromGitHub {
+          owner = "damalsk";
+          repo = "damalsksoundpack";
+          rev = "a69ed2bb65350b2700004b42fca02a5559fd8dad";
+          hash = "sha256-/wEkfXy1Ov/sH3gyFUWFTQ2iH74+yI6+nzlQSa3a2eg=";
+        };
+      };
       myCdda =
         { hasTiles }:
-        myCddaPackages.dark-days-ahead.overrideAttrs (
+        (myCddaPackages.dark-days-ahead.overrideAttrs (
           final: prev: {
             pname = if hasTiles then "${prev.pname}-tiles" else prev.pname;
             version = "0.I-2026-03-23-0402";
@@ -23,7 +33,8 @@
             patches = [ ./cdda-locale-path.patch ];
             inherit hasTiles;
           }
-        );
+        )).withMods
+          (_: [ damalskSoundpack ]);
     in
     {
       packages = {
