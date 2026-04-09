9ded4a5ea2c2189a09d6d222554c23085edd36a0
Tom van Dijk <18gatenmaker6@gmail.com>
2026-04-09 23:20:36 +0200

    feat(nvim/localconfig): more names

--- stow/nvim/dot-local/share/nvim/site/plugin/localconfig.lua
+++ stow/nvim/dot-local/share/nvim/site/plugin/localconfig.lua
@@ -5,17 +5,20 @@ local group = vim.api.nvim_create_augroup("LocalConfig", { clear = true })
 vim.api.nvim_create_autocmd({ 'DirChanged', 'VimEnter' }, {
     callback = function()
         local cwd = vim.fn.getcwd(0)
-        local rc_path = vim.fs.joinpath(cwd, ".nvimrc.lua")
-        ---@diagnostic disable-next-line: undefined-field
-        if vim.uv.fs_stat(rc_path) then
-            vim.ui.select(
-                { 'source', 'edit', 'skip' }, {
-                    prompt = (".nvimrc.lua found in %s! What to do?"):format(cwd),
-                },
-                function(choice)
-                    if choice == nil or choice == 'skip' then return end
-                    (vim.cmd[choice] or function() end)(rc_path)
-                end)
+        for _, rc_name in ipairs { ".nvimrc.lua", ".nvimrc.vim", ".nvimrc", ".vimrc" } do
+            local rc_path = vim.fs.joinpath(cwd, rc_name)
+            ---@diagnostic disable-next-line: undefined-field
+            if vim.uv.fs_stat(rc_path) then
+                vim.ui.select(
+                    { 'source', 'edit', 'skip' }, {
+                        prompt = ("%s found in %s! What to do?"):format(rc_name, cwd),
+                    },
+                    function(choice)
+                        if choice == nil or choice == 'skip' then return end
+                        (vim.cmd[choice] or function(_) end)(rc_path)
+                    end)
+                break
+            end
         end
     end,
     group = group,
