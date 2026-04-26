46fffd7c14556b8f33764a647e32a2e488ec76f7
Tom van Dijk <18gatenmaker6@gmail.com>
2026-04-26 02:17:20 +0200

    chore(workstation): dedup flatpaks from nixpkgs

--- modules/profiles/workstation.nix
+++ modules/profiles/workstation.nix
@@ -23,8 +23,6 @@
       # assumes nix-flatpak is available
       services.flatpak.packages = [
         "com.obsproject.Studio"
-        "org.gnome.World.PikaBackup"
-        "dev.overlayed.Overlayed"
       ];
 
       environment.systemPackages = with pkgs; [
