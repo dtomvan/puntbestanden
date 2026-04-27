0d86b91d4a72a5226f4c9ab068b60914da589eb9
Tom van Dijk <18gatenmaker6@gmail.com>
2026-04-27 18:26:48 +0200

    Revert "myCdda: 0.I-2026-03-23-0402 -> 0.I-unstable-2026-04-15"
    This reverts commit 75be55837d8114d799d3fcc149078ffe3dc13b4f.
    
    Broken for now

--- modules/community/gaming/cdda.nix
+++ modules/community/gaming/cdda.nix
@@ -23,12 +23,12 @@
         (myCddaPackages.dark-days-ahead.overrideAttrs (
           final: prev: {
             pname = if hasTiles then "${prev.pname}-tiles" else prev.pname;
-            version = "0.I-unstable-2026-04-15";
+            version = "0.I-2026-03-23-0402";
             src = pkgs.fetchFromGitHub {
               owner = "CleverRaven";
               repo = "Cataclysm-DDA";
-              rev = "46730b6e7a9ed1c5eebf065a40760183d8d39fc2";
-              hash = "sha256-4RjYr3GNIeIKiSLJ046fALoogDcQlq+ouroYmASwC80=";
+              tag = "cdda-${final.version}";
+              hash = "sha256-sfoZ8ey/hr0NGTJr/ywr/0/S6UcsSHkJoRfPaq7tfMc=";
             };
             patches = [ ./cdda-locale-path.patch ];
             inherit hasTiles;
