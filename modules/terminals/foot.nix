6765cc049d1c6aac1ad76ea27c89f494f2871b6e
Tom van Dijk <18gatenmaker6@gmail.com>
2026-04-27 17:27:40 +0200

    feat(foot): run zellij

--- modules/terminals/foot.nix
+++ modules/terminals/foot.nix
@@ -20,6 +20,7 @@
         settings.main = {
           term = "xterm-256color";
           font = "${cfg.font.family}:size=${toString cfg.font.size}";
+          shell = lib.getExe config.programs.zellij.finalPackage;
         };
       };
     };
