7860a848f67e395a6441f2f16836635c99af6b67
Tom van Dijk <18gatenmaker6@gmail.com>
2026-05-03 11:06:11 +0200

    chore!: boot.zfs.forceImportRoot new default

---
+++ modules/workarounds/nixos-deprecations.nix
@@ -0,0 +1,7 @@
+{
+  # trace: evaluation warning: `boot.zfs.forceImportRoot` is using the default
+  # value of `true`. It is highly recommended to set it to `false`, the new
+  # default from 26.11 on, to reduce the risk of data loss. Alternatively, you
+  # can silence this warning by explicitly setting it to `true`.
+  flake.modules.nixos.profiles.base.boot.zfs.forceImportRoot = false;
+}
