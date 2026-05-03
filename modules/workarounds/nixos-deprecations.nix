5f784478bfedfdea9874c2acc33977c92fb57787
Tom van Dijk <18gatenmaker6@gmail.com>
2026-05-03 13:21:06 +0200

    fixup! chore!: boot.zfs.forceImportRoot new default

--- modules/workarounds/nixos-deprecations.nix
+++ modules/workarounds/nixos-deprecations.nix
@@ -3,5 +3,7 @@
   # value of `true`. It is highly recommended to set it to `false`, the new
   # default from 26.11 on, to reduce the risk of data loss. Alternatively, you
   # can silence this warning by explicitly setting it to `true`.
-  flake.modules.nixos.profiles.base.boot.zfs.forceImportRoot = false;
+  flake.modules.nixos = {
+    profiles-base.boot.zfs.forceImportRoot = false;
+  };
 }
