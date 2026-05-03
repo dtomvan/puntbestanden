6a4ea66d83e62a13d2bd49fbabc4a1071f03c762
Tom van Dijk <18gatenmaker6@gmail.com>
2026-05-03 13:23:02 +0200

    chore!: virtualisation.incus.bucketSupport = false
    Disables minio as it's unmaintained
    
    See https://github.com/NixOS/nixpkgs/pull/514811 and https://github.com/lxc/incus/pull/3258

--- modules/workarounds/nixos-deprecations.nix
+++ modules/workarounds/nixos-deprecations.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 {
   # trace: evaluation warning: `boot.zfs.forceImportRoot` is using the default
   # value of `true`. It is highly recommended to set it to `false`, the new
@@ -5,5 +6,11 @@
   # can silence this warning by explicitly setting it to `true`.
   flake.modules.nixos = {
     profiles-base.boot.zfs.forceImportRoot = false;
+
+    # new option in unstable which gets a default with stateVersion >= 26.11,
+    # which I don't (nessecarily) have (yet)
+    # the option enables minio which is unmaintained. Hence the new default
+    # will be false. Setting that explicitly.
+    virt-incus.virtualisation.incus.bucketSupport = lib.mkDefault false;
   };
 }
