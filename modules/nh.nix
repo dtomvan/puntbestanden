c89878d03748ba19fa2e3565faa164a858973045
Tom van Dijk <18gatenmaker6@gmail.com>
2026-05-03 17:04:28 +0200

    chore(nh): show activation logs

--- modules/nh.nix
+++ modules/nh.nix
@@ -1,11 +1,14 @@
 {
-  flake.modules.nixos.profiles-base.programs.nh = {
-    enable = true;
-    flake = "/home/tomvd/puntbestanden/";
-    clean = {
+  flake.modules.nixos.profiles-base = {
+    programs.nh = {
       enable = true;
-      dates = "Sun *-*-* 18:00:00";
-      extraArgs = "--keep 5 --keep-since 14d --nogcroots";
+      flake = "/home/tomvd/puntbestanden/";
+      clean = {
+        enable = true;
+        dates = "Sun *-*-* 18:00:00";
+        extraArgs = "--keep 5 --keep-since 14d --nogcroots";
+      };
     };
+    environment.variables.NH_SHOW_ACTIVATION_LOGS = 1;
   };
 }
