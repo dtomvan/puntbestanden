2e5cfdaf2f91da878eee89a29bc87a3ae0f381f0
Tom van Dijk <18gatenmaker6@gmail.com>
2026-05-09 19:33:24 +0200

    chore(files): remove identity `files.files`
    That is, definitions that just copy some fixed contents to some file. it
    isn't useful.

--- modules/top-level/devshell.nix
+++ modules/top-level/devshell.nix
@@ -39,23 +39,5 @@
           '';
         }) self'.apps;
       };
-
-      files.files = [
-        {
-          path_ = ".envrc.recommended";
-          drv = pkgs.writeText ".envrc.recommended" ''
-            if ! has nix_direnv_version || ! nix_direnv_version 3.1.0; then
-              source_url "https://raw.githubusercontent.com/nix-community/nix-direnv/3.1.0/direnvrc" "sha256-yMJ2OVMzrFaDPn7q8nCBZFRYpL/f0RcHzhmw/i6btJM="
-            fi
-
-            use flake
-          '';
-        }
-      ];
     };
-
-  text.gitignore = ''
-    /.direnv
-    /.tmp
-  '';
 }
