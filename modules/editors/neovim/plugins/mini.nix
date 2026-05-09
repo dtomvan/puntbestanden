6a7faf2c06ddf1fbc58eca6f119e48682dea2a42
Tom van Dijk <18gatenmaker6@gmail.com>
2026-05-10 00:27:02 +0200

    fix(nixvim/minimal): add git dependency for mini.git

--- modules/editors/neovim/plugins/mini.nix
+++ modules/editors/neovim/plugins/mini.nix
@@ -1,42 +1,51 @@
 {
-  flake.modules.nixvim.minimal.plugins.mini = {
-    enable = true;
-    modules = {
-      ai = { };
-      basics = {
-        options = {
-          basic = true;
-          extra_ui = true;
-        };
-        mappings.windows = true;
-        autocommands = {
-          basic = true;
-        };
+  flake.modules.nixvim.minimal =
+    { pkgs, ... }:
+    {
+      dependencies.git = {
+        enable = true;
+        packageFallback = true;
+        package = pkgs.gitMinimal;
       };
-      bracketed = { };
-      comment = { };
-      icons = { };
-      indentscope = { };
-      git = { };
-      notify = { };
-      sessions = { };
-      statusline = { };
-      # pairs = {};
-      surround = {
-        mappings = {
-          app = "ys";
-          delete = "ds";
-          find = "";
-          find_left = "";
-          highlight = "";
-          replace = "cs";
-          update_n_lines = "";
-          suffix_last = "";
-          suffix_next = "";
+      plugins.mini = {
+        enable = true;
+        modules = {
+          ai = { };
+          basics = {
+            options = {
+              basic = true;
+              extra_ui = true;
+            };
+            mappings.windows = true;
+            autocommands = {
+              basic = true;
+            };
+          };
+          bracketed = { };
+          comment = { };
+          icons = { };
+          indentscope = { };
+          git = { };
+          notify = { };
+          sessions = { };
+          statusline = { };
+          # pairs = {};
+          surround = {
+            mappings = {
+              app = "ys";
+              delete = "ds";
+              find = "";
+              find_left = "";
+              highlight = "";
+              replace = "cs";
+              update_n_lines = "";
+              suffix_last = "";
+              suffix_next = "";
+            };
+            search_method = "cover_or_next";
+          };
         };
-        search_method = "cover_or_next";
+        mockDevIcons = true;
       };
     };
-    mockDevIcons = true;
-  };
 }
