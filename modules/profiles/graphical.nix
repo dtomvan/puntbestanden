895564e8cbe6e428b50decf85f49b890e2d9b4f3
Tom van Dijk <18gatenmaker6@gmail.com>
2026-05-10 16:11:16 +0200

    fix(graphical): set correct cursor theme name

--- modules/profiles/graphical.nix
+++ modules/profiles/graphical.nix
@@ -45,7 +45,7 @@ in
         home.pointerCursor = {
           enable = true;
           package = pkgs.kdePackages.breeze;
-          name = "Breeze";
+          name = "breeze_cursors";
           size = 24;
           gtk.enable = true;
           x11.enable = true;
