c1f5163be473a6f9d3f274e38ca57b2774f77a44
Tom van Dijk <18gatenmaker6@gmail.com>
2026-05-10 21:26:08 +0200

    fix(feather): revert kernel to LTS, disable lockKernelModules (broken WiFi)

--- modules/boot/kernel.nix
+++ modules/boot/kernel.nix
@@ -1,7 +1,15 @@
 {
-  flake.modules.nixos.profiles-base =
-    { pkgs, ... }:
-    {
-      boot.kernelPackages = pkgs.linuxPackages_latest;
-    };
+  flake.modules.nixos = {
+    profiles-base =
+      { pkgs, lib, ... }:
+      {
+        boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+      };
+
+    feather =
+      { pkgs, ... }:
+      {
+        boot.kernelPackages = pkgs.linuxPackages;
+      };
+  };
 }
--- modules/workarounds/dirty-frag.nix
+++ modules/workarounds/dirty-frag.nix
@@ -1,22 +1,25 @@
 {
-  flake.modules.nixos.profiles-base =
-    { pkgs, lib, ... }:
-    let
-      vulnerableModules = [
-        "esp4"
-        "esp6"
-        "rxrpc"
-      ];
-    in
-    {
-      # disable loading kernel modules after we are fully initialized. This
-      # does break apps like droidcam because it modprobes v4l2loopback JIT.
-      security.lockKernelModules = true;
+  flake.modules.nixos = {
+    profiles-base =
+      { pkgs, lib, ... }:
+      let
+        vulnerableModules = [
+          "esp4"
+          "esp6"
+          "rxrpc"
+        ];
+      in
+      {
+        # disable loading kernel modules after we are fully initialized. This
+        # does break apps like droidcam because it modprobes v4l2loopback JIT.
+        security.lockKernelModules = lib.mkDefault true;
 
-      # ...and also just get rid of these modules since we don't need it
-      boot.extraModprobeConfig = lib.concatMapStringsSep "\n" (
-        m: "install ${m} ${lib.getExe' pkgs.coreutils "false"}"
-      ) vulnerableModules;
-      boot.blacklistedKernelModules = vulnerableModules;
-    };
+        # ...and also just get rid of these modules since we don't need it
+        boot.extraModprobeConfig = lib.concatMapStringsSep "\n" (
+          m: "install ${m} ${lib.getExe' pkgs.coreutils "false"}"
+        ) vulnerableModules;
+        boot.blacklistedKernelModules = vulnerableModules;
+      };
+    feather.security.lockKernelModules = false;
+  };
 }
