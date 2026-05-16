ef9a52148af10bce56e4874ee2f2756b8311cd7a
Tom Oostveen <tomoostveen.06@gmail.com>
2026-05-16 16:23:55 +0200

    feat(profiles-base): add ip forwarding

--- modules/services/networking/networkmanager.nix
+++ modules/services/networking/networkmanager.nix
@@ -1,3 +1,10 @@
 {
-  flake.modules.nixos.profiles-base.networking.networkmanager.enable = true;
+  flake.modules.nixos.profiles-base = {
+    networking.networkmanager.enable = true;
+    boot.kernel.sysctl = {
+      "net.ipv4.ip_forward" = 1;
+      "net.ipv4.conf.all.forwarding" = 1;
+      "net.ipv6.conf.all.forwarding" = 1;
+    };
+  };
 }
