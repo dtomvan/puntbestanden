bee8d22a17866aa7fe8ff42d23e7dd08f6bc5bfe
Tom van Dijk <18gatenmaker6@gmail.com>
2026-05-16 10:53:08 +0200

    refactor: split nix-common into nix-command and nix-sensible

--- modules/nix/sensible.nix
+++ modules/nix/sensible.nix
@@ -1,5 +1,5 @@
 {
-  flake.modules.nixos.nix-common.nix.settings = {
+  flake.modules.nixos.nix-sensible.nix.settings = {
     connect-timeout = 5;
     min-free = 128 * 1000 * 1000;
     max-free = 1 * 1000 * 1000 * 1000;
