a87bd31074dfdcd3957d7fb39d267a386b98e4fc
Tom Oostveen <tomoostveen.06@gmail.com>
2026-05-17 10:03:06 +0200

    chore(files): vendor mightyiam/files
    - They made a bunch of breaking changes in trivial parts of the code
    - They removed the `flakeModules` output
    - They pulled in nixpkgs (??)
    - Found that I can simplify it by a lot

--- modules/meta/files.nix
+++ modules/meta/files.nix
@@ -1,20 +1,63 @@
-{ inputs, ... }:
 {
-  flake-file.inputs = {
-    files.url = "github:mightyiam/files";
-  };
-
-  imports = [
-    inputs.files.flakeModules.default
-  ];
-
-  perSystem =
-    { config, ... }:
+  flake-parts-lib,
+  lib,
+  self,
+  ...
+}:
+let
+  inherit (lib)
+    mkOption
+    escapeShellArg
+    concatMapAttrsStringSep
+    mapAttrs'
+    ;
+  inherit (lib.types)
+    attrsOf
+    package
+    either
+    pathInStore
+    ;
+in
+{
+  options.perSystem = flake-parts-lib.mkPerSystemOption (
+    { pkgs, config, ... }:
+    let
+      inherit (config) files;
+    in
     {
-      apps.write-files = {
-        type = "app";
-        program = config.files.writer.drv;
-        meta.description = "Write all files.files to the correct places";
+      options.files = mkOption {
+        default = { };
+        type = attrsOf (either package pathInStore);
       };
-    };
+
+      config = {
+        apps.write-files = {
+          type = "app";
+          program = pkgs.writeShellApplication {
+            name = "write-files";
+            runtimeInputs = [ pkgs.gitMinimal ];
+            preferLocalBuild = true;
+            text = ''
+              pushd "$(git rev-parse --show-toplevel)"
+            ''
+            + (concatMapAttrsStringSep "\n" (p: drv: ''
+              mkdir -p "$(dirname ${escapeShellArg p})"
+              cp ${drv} ${escapeShellArg p}
+            '') files);
+          };
+
+          meta.description = "Write all files.files to the correct places";
+        };
+
+        checks = mapAttrs' (p: drv: {
+          name = "files/${p}";
+          value = pkgs.runCommand "files-check-${p}" { nativeBuildInputs = [ pkgs.difftastic ]; } ''
+            difft --exit-code --display inline "${drv}" ${escapeShellArg "${self}/${p}"}
+            touch $out
+          '';
+        }) files;
+      };
+    }
+  );
+
 }
