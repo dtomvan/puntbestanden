ed33613ccf4fcf70d2a12559eb8558974208bae4
Tom Oostveen <tomoostveen.06@gmail.com>
2026-05-23 23:00:04 +0200

    feat: moar bookmarks

--- modules/programs/firefox/bookmarks.nix
+++ modules/programs/firefox/bookmarks.nix
@@ -10,36 +10,124 @@
             url = "about:config";
           }
           {
-            name = "nixpkgs";
-            url = "https://github.com/NixOS/nixpkgs/";
-          }
-          {
-            name = "nixpkgs pulls";
-            url = "https://github.com/NixOS/nixpkgs/pulls?q=involves:dtomvan";
-          }
-          {
             name = "Add to Miniflux";
             url = "javascript:location.href='https://rss.toostveen.nl/bookmarklet?uri='+encodeURIComponent(window.location.href)";
           }
           {
-            name = "Nix manual";
-            url = "https://nix.dev/manual/nix/latest/";
+            name = "Nix";
+            bookmarks = [
+              {
+                name = "homepage";
+                url = "https://nixos.org/";
+              }
+              {
+                name = "wiki";
+                url = "https://wiki.nixos.org/";
+              }
+              {
+                name = "search";
+                url = "https://search.nixos.org/";
+              }
+              {
+                name = "status";
+                url = "https://status.nixos.org/";
+              }
+              {
+                name = "discourse";
+                url = "https://discourse.nixos.org/";
+              }
+              {
+                name = "nixpkgs";
+                url = "https://github.com/NixOS/nixpkgs/";
+              }
+              {
+                name = "nixpkgs pulls";
+                url = "https://github.com/NixOS/nixpkgs/pulls?q=involves:dtomvan";
+              }
+              {
+                name = "NUR";
+                url = "https://nur.nix-community.org";
+              }
+              {
+                name = "Nix manual";
+                url = "https://nix.dev/manual/nix/latest/";
+              }
+              {
+                name = "NüschtOS search";
+                url = "https://search.nüschtos.de/";
+              }
+              {
+                name = "Me on repology";
+                url = "https://repology.org/maintainer/18gatenmaker6%40gmail.com";
+              }
+              {
+                name = "Nixpkgs security tracker";
+                url = "https://tracker.security.nixos.org/";
+              }
+            ];
           }
           {
-            name = "NüschtOS search";
-            url = "https://search.nüschtos.de/";
+            name = "infra";
+            bookmarks = [
+              {
+                name = "fs";
+                url = "https://fs.toostveen.nl";
+              }
+              {
+                name = "rss";
+                url = "https://rss.toostveen.nl";
+              }
+              {
+                name = "grafana";
+                url = "https://grafana.toostveen.nl";
+              }
+              {
+                name = "prometheus";
+                url = "https://prometheus.toostveen.nl";
+              }
+            ];
           }
           {
-            name = "Me on repology";
-            url = "https://repology.org/maintainer/18gatenmaker6%40gmail.com";
+            name = "Forges";
+            bookmarks = [
+              {
+                name = "tom";
+                url = "https://git.toostveen.nl";
+              }
+              {
+                name = "bart";
+                url = "https://git.bartoostveen.nl";
+              }
+              {
+                name = "elisaado";
+                url = "https://git.elisaado.nl";
+              }
+              {
+                name = "codeberg";
+                url = "https://codeberg.org";
+              }
+              {
+                name = "lix";
+                url = "https://git.lix.systems";
+              }
+            ];
           }
           {
-            name = "Nixpkgs security tracker";
-            url = "https://tracker.security.nixos.org/";
-          }
-          {
-            name = "Nix package versions";
-            url = "https://lazamar.co.uk/nix-versions/";
+            name = "social";
+            bookmarks = [
+              {
+                name = "toot";
+                url = "https://toot.cat";
+              }
+              {
+                name = "lobsters";
+                url = "https://lobste.rs";
+              }
+              {
+                name = "matrix";
+                url = "https://app.cinny.in";
+              }
+            ];
           }
         ];
       }
