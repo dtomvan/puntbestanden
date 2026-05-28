962e74601f68095ba3df83d3713d1b480f0b70af
Tom Oostveen <tomoostveen.06@gmail.com>
2026-05-29 01:02:45 +0200

    chore: remove garnix cache
    garnix is going down, let's not rely on the cache anymore in any way

--- flake.nix
+++ flake.nix
@@ -19,7 +19,6 @@
       "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
 
