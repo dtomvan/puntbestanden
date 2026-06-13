f284dc4834d1fd386d01ef34b03b5b19cbaf923f
Tom Oostveen <tomoostveen.06@gmail.com>
2026-06-14 00:27:32 +0200

    fix(profiles-workstation/pb): correct plaintext detection

--- modules/services/copyparty.nix
+++ modules/services/copyparty.nix
@@ -362,13 +362,18 @@ in
           tmp="$(mktemp)"
           cat > "$tmp"
           ext="$(file --brief --extension "$tmp" | awk -F/ '{ print $1 }')"
+          mime="$(file --brief --mime-type "$tmp")"
           case "$ext" in
-            txt)
-              drop="paste";;
             png | jpg | jpeg)
               drop="scrot";;
             *)
-              drop="priv";;
+              if [[ "$mime" =~ ^text/ ]]; then
+                drop="paste"
+                ext="txt"
+              else
+                drop="priv"
+              fi
+              ;;
           esac
           mv "$tmp" "$tmp.$ext"
           u2c -u -a "$(sops decrypt ${../../secrets/copyparty.secret})" "''${1:-"https://${defaultDomain}/$drop"}" "$tmp.$ext"
