99176d98bdc0854dd3a9765fa85a2a4a675f5014
Tom Oostveen <tomoostveen.06@gmail.com>
2026-06-26 01:22:30 +0200

    feat(get-disk-id): allow raw output

--- modules/disko.nix
+++ modules/disko.nix
@@ -77,19 +77,40 @@
           writeShellApplication,
           util-linux,
           jq,
+          gawk,
+          coreutils,
         }:
         writeShellApplication {
           name = "get-disk-id";
           runtimeInputs = [
             util-linux
             jq
+            gawk
+            coreutils
           ];
           text = ''
-            blocks="$(lsblk -o MOUNTPOINTS,ID -J)"
-            for mount in / /mnt "$@"; do
-              printf "mountpoint %s: " "$mount"
-              jq -r '.blockdevices | map(select(.mountpoints | index("'"$mount"'")) | "/dev/disk/by-id/\(.id)")[]' <<< "$blocks"
-            done
+            doit () {
+              blocks="$(lsblk -o MOUNTPOINTS,ID -J)"
+              for mount in /mnt / "$@"; do
+                printf "mountpoint %s: " "$mount"
+                # ensure consistent output: jq never outputs a newline, because
+                # halt_error(0) prints the string without a newline (to stderr
+                # but we redirect), with exit code 0, if the input made it thru
+                # the select, and bash always prints a newline afterwards. That
+                # way we don't have to trim jq's output
+                jq --arg mount "$mount" \
+                  -r '.blockdevices | map(select(.mountpoints | index($mount)) | "/dev/disk/by-id/\(.id)" | halt_error(0))[]' \
+                  <<< "$blocks" \
+                  2>&1
+                echo
+              done
+            }
+            if [ "''${1:-}" = --raw ]; then
+              shift
+              doit "$@" | awk '(NF > 2) { print $NF }' | head -n1
+            else
+              doit "$@"
+            fi
           '';
         }
       ) { };
