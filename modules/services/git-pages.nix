79ec83b7e8e3cdcbd60c86f700d2ec38e4e709c4
Tom Oostveen <tomoostveen.06@gmail.com>
2026-06-29 20:42:49 +0200

    chore(services-git-pages): TDD

--- modules/services/git-pages.nix
+++ modules/services/git-pages.nix
@@ -32,6 +32,80 @@ in
     final: prev: {
       git-pages = prev.git-pages.overrideAttrs {
         passthru = prev.git-pages.passthru // {
+          tests.modularService = final.testers.runNixOSTest (
+            { pkgs, ... }:
+            let
+              testSite = pkgs.runCommand "git-pages-testsite.tar" { } ''
+                echo It works! > index.html
+                tar cvf $out index.html
+              '';
+            in
+            {
+              name = "git-pages-modular-service";
+
+              nodes.machine = {
+                environment.systemPackages = [ pkgs.curl ];
+
+                system.services.git-pages = {
+                  _class = "service";
+                  imports = [ pkgs.git-pages.services.default ];
+                  git-pages = {
+                    settings.server = {
+                      pages = "tcp/:3000";
+                      caddy = "tcp/:3001";
+                      metrics = "tcp/:3002";
+                    };
+                  };
+                  systemd.service.environment.PAGES_INSECURE = "1";
+                };
+
+                services.caddy = {
+                  enable = true;
+                  configFile =
+                    builtins.toFile "Caddyfile"
+                      # from upstream
+                      # caddy
+                      ''
+                        {
+                            admin off
+
+                            persist_config off
+
+                            auto_https disable_redirects
+
+                            on_demand_tls {
+                                permission http http://localhost:3001
+                            }
+                        }
+
+                        https://, http:// {
+                          tls {
+                              on_demand
+                          }
+
+                          reverse_proxy http://localhost:3000
+                        }
+                      '';
+                };
+
+                networking.firewall.allowedTCPPorts = [ 80 ];
+              };
+
+              testScript = ''
+                start_all()
+
+                machine.wait_for_unit("caddy.service")
+                machine.wait_for_open_port(80)
+                machine.wait_for_unit("git-pages.service")
+                machine.fail("curl -f http://localhost:3000/.git-pages/health")
+                machine.succeed("curl -f http://localhost:3000/ -X PUT --data-binary @${testSite} --header 'Content-Type: application/x-tar'")
+                machine.succeed("sleep 1")
+                machine.succeed("curl -f http://localhost:3000/.git-pages/health")
+                machine.succeed("curl -f http://localhost:3000/ | grep -F 'It works!'")
+              '';
+            }
+          );
+
           services.default =
             { config, ... }:
             let
