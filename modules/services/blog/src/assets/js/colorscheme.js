0607e58dc55e0510360d7ae7af4b94b61e0bb0ef
Tom Oostveen <tomoostveen.06@gmail.com>
2026-07-11 19:43:50 +0200

    feat(blog): remember preferences

--- modules/services/blog/src/assets/js/colorscheme.js
+++ modules/services/blog/src/assets/js/colorscheme.js
@@ -1,8 +1,5 @@
-// very cursed. the 2 link tags below load in either the light or
-// dark color scheme depending on the system preference. If it's
-// unavailable the below block will fallback to light. Afterwards make
-// the toggle that will remove the "default colorscheme" stylesheets
-// and insert a new one that points to the user override.
+// load in light as a fallback when the system colorscheme isn't provided by
+// the browser
 if (window.matchMedia("(prefers-color-scheme: dark)").media === "not all") {
   document.documentElement.style.display = "none";
   document.head.insertAdjacentHTML(
@@ -11,26 +8,39 @@ if (window.matchMedia("(prefers-color-scheme: dark)").media === "not all") {
   );
 }
 
-const preferredColorScheme = localStorage.getItem("preferred-color-scheme");
-window.colorScheme =
-  preferredColorScheme != null
-    ? preferredColorScheme
-    : window.matchMedia("(prefers-color-scheme: dark)").matches
+// point #color-override to the new colorscheme, creating it if it doesn't exist.
+function activateColorScheme(colorScheme) {
+  let override = document.getElementById("color-override");
+  if (override == null) {
+    document.head.insertAdjacentHTML(
+      "beforeend",
+      `<link id="color-override" rel="stylesheet" href="/assets/css/${colorScheme}.css">`,
+    );
+  } else {
+    override.href = `/assets/css/${colorScheme}.css`;
+  }
+
+  document
+    .querySelectorAll('link[rel="stylesheet"].system-colorscheme')
+    .forEach((n) => document.head.removeChild(n));
+}
+
+addEventListener("DOMContentLoaded", (event) => {
+  const preferredColorScheme = localStorage.getItem("preferred-color-scheme");
+  if (preferredColorScheme != null) {
+    window.colorScheme = preferredColorScheme;
+    activateColorScheme(preferredColorScheme);
+  } else {
+    window.colorScheme = window.matchMedia("(prefers-color-scheme: dark)").matches
       ? "dark"
       : "light";
-document.head.insertAdjacentHTML(
-  "beforeend",
-  `<link id="color-override" rel="stylesheet" href="/assets/css/${window.colorScheme}.css">`,
-);
-addEventListener("DOMContentLoaded", (event) => {
+  }
+
   const toggleColorScheme = () => {
     window.colorScheme = window.colorScheme == "dark" ? "light" : "dark";
     localStorage.setItem("preferred-color-scheme", window.colorScheme);
 
-    document.getElementById("color-override").href = `/assets/css/${window.colorScheme}.css`;
-    document
-      .querySelectorAll('link[rel="stylesheet"].system-colorscheme')
-      .forEach((n) => document.head.removeChild(n));
+    activateColorScheme(window.colorScheme);
   };
   document.querySelector("button.light-dark-toggle").onclick = toggleColorScheme;
 });
---
+++ modules/services/blog/src/assets/js/mono.js
@@ -0,0 +1,11 @@
+function toggleMonospace() {
+  document.body.classList.toggle("mono");
+  let newPref = document.body.classList.contains("mono");
+  localStorage.setItem("wants-monospace", newPref);
+}
+addEventListener("DOMContentLoaded", (event) => {
+  const monoPref = localStorage.getItem("wants-monospace");
+  if (monoPref === "true") {
+    document.body.classList.add("mono");
+  }
+});
