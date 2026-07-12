44e664d67aa83506118ea174c09d85f9b8c7adf8
Tom Oostveen <tomoostveen.06@gmail.com>
2026-07-12 15:15:00 +0200

    chore: matrix-continuwuity: 0.5.10 -> 26.6.1
    HACK: copy-pasted the package

---
+++ modules/workarounds/c10y-26.6.nix
@@ -0,0 +1,108 @@
+let
+  package =
+    {
+      lib,
+      rustPlatform,
+      fetchFromGitea,
+      pkg-config,
+      bzip2,
+      zstd,
+      stdenv,
+      rocksdb,
+      nix-update-script,
+      testers,
+      matrix-continuwuity,
+      rust-jemalloc-sys-unprefixed,
+      liburing,
+      nixosTests,
+    }:
+    let
+      rocksdb' =
+        (rocksdb.override {
+          # rocksdb does not support prefixed jemalloc, which is required on darwin
+          enableJemalloc = !stdenv.hostPlatform.isDarwin;
+          jemalloc = rust-jemalloc-sys-unprefixed;
+        }).overrideAttrs
+          (
+            final: old: {
+              version = "11.1.1";
+              src = fetchFromGitea {
+                domain = "forgejo.ellis.link";
+                owner = "continuwuation";
+                repo = "rocksdb";
+                rev = "3756b2b905e13216d8b56bcc783d814e7b073aff";
+                hash = "sha256-rSv4fr2bf9JJwdodgeuPCuceeh7k97KVxrAOC0wyPQY=";
+              };
+
+              patches = [ ];
+            }
+          );
+    in
+    rustPlatform.buildRustPackage (finalAttrs: {
+      pname = "matrix-continuwuity";
+      version = "26.6.1";
+
+      src = fetchFromGitea {
+        domain = "forgejo.ellis.link";
+        owner = "continuwuation";
+        repo = "continuwuity";
+        tag = "v${finalAttrs.version}";
+        hash = "sha256-sfUxhBPf0P46Xf0lcZFDj23cuZ9apJDyTQPa97jlcSY=";
+      };
+
+      cargoHash = "sha256-Qmw9Xum+Osu/4kbVqJP79gbtimpV2P7SkCMuQWyBYxg=";
+
+      nativeBuildInputs = [
+        pkg-config
+        rustPlatform.bindgenHook
+      ];
+
+      buildInputs = [
+        bzip2
+        zstd
+        rust-jemalloc-sys-unprefixed
+        liburing
+      ];
+
+      env = {
+        ZSTD_SYS_USE_PKG_CONFIG = true;
+        ROCKSDB_INCLUDE_DIR = "${rocksdb'}/include";
+        ROCKSDB_LIB_DIR = "${rocksdb'}/lib";
+      };
+
+      passthru = {
+        rocksdb = rocksdb'; # make used rocksdb version available (e.g., for backup scripts)
+        updateScript = nix-update-script { };
+        tests = {
+          version = testers.testVersion {
+            inherit (finalAttrs) version;
+            package = matrix-continuwuity;
+          };
+        }
+        // lib.optionalAttrs stdenv.hostPlatform.isLinux {
+          inherit (nixosTests) matrix-continuwuity;
+        };
+      };
+
+      meta = {
+        description = "Matrix homeserver written in Rust, forked from conduwuit";
+        homepage = "https://continuwuity.org/";
+        changelog = "https://forgejo.ellis.link/continuwuation/continuwuity/releases/tag/v${finalAttrs.version}";
+        license = lib.licenses.asl20;
+        maintainers = with lib.maintainers; [
+          bartoostveen
+          nyabinary
+          snaki
+        ];
+        # Not a typo, continuwuity is a drop-in replacement for conduwuit.
+        mainProgram = "conduwuit";
+      };
+    });
+in
+{
+  pkgs-overlays = [
+    (_: prev: {
+      matrix-continuwuity = prev.callPackage package { };
+    })
+  ];
+}
