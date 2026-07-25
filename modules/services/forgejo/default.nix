ea80fe16e1a7d1e7bac12f7b97f09d2b13c5127d
Tom Oostveen <tomoostveen.06@gmail.com>
2026-07-25 12:54:01 +0200

    Revert "chore(services-forgejo): disable ratelimiting"
    This reverts commit 887d3c3b1e300fddca241f61e63cb0d3ec71a42c.

--- modules/services/forgejo/default.nix
+++ modules/services/forgejo/default.nix
@@ -146,9 +146,6 @@
                 access_log off;
               '';
             };
-            "/v2".extraConfig = ''
-              limit_req off;
-            '';
           }
           // lib.optionalAttrs cfg.iocaine.enable {
             "@fallback".proxyPass = "http://localhost:${toString cfg.httpPort}";
