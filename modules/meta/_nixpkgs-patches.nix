44067a14f876910aa602352a298b8e92babfbe8f
Tom Oostveen <tomoostveen.06@gmail.com>
2026-07-29 23:54:13 +0200

    chore(pkgs): remove old patch

--- modules/meta/_nixpkgs-patches.nix
+++ modules/meta/_nixpkgs-patches.nix
@@ -1,10 +1,4 @@
 { fetchpatch2, ... }: [
-  # fix modular services
-  (fetchpatch2 {
-    url = "https://github.com/NixOS/nixpkgs/commit/979b6b58c3d421eefe520bb10e7cd1794ecabfdd.patch";
-    excludes = [ "doc" ];
-    hash = "sha256-RO1PYEIX1NPKq0rCXsHzjuZTcl/2QAbCLCnXReEYErs=";
-  })
   # nixos/forgejo-runner: init
   (fetchpatch2 {
     url = "https://github.com/NixOS/nixpkgs/commit/48f87539d9cfc5ca4f7f6913dfcf51d24e849fcb.patch";
