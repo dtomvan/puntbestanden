cc032ad625a71cd209874077656c89576aae62f3
Tom Oostveen <tomoostveen.06@gmail.com>
2026-09-07 10:30:55 +0200

    fix!(profiles-base): services.journald.extraConfig is gone

--- modules/services/journald.nix
+++ modules/services/journald.nix
@@ -1,6 +1,6 @@
 {
-  flake.modules.nixos.profiles-base.services.journald.extraConfig = ''
-    SystemMaxUse=250M
-    SystemMaxFileSize=50M
-  '';
+  flake.modules.nixos.profiles-base.services.journald.settings.Journal = {
+    SystemMaxUse = "250M";
+    SystemMaxFileSize = "50M";
+  };
 }
